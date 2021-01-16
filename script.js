7586614eb56f298bd1d91ea1ed72158508e1bb20
RasputinForever <erik.justin.portillo@gmail.com>
2021-01-16 12:41:26 -0800

    Starting SudoCode

---
+++ script.js
@@ -0,0 +1,24 @@
+// use node to create a ReadMe
+//Ask for these things
+// 1. Main Title
+// 2. Introduction
+// 3. How many additional sections
+    // for each additional...
+    // section name
+    // section text contents
+    // images
+    // links
+
+//auto generation creates the ReadMe's
+    //text formats
+    //link formats
+    //image formats
+    //table of contents
+
+//All inputs go to an object. based on the object structure and object lables, the ReadMe is generated. 
+//Step 1: Inqurirer
+//Step 2: fs
+
+//Notes
+//ReadMe Generator SHOULD be able to EDIT an existing readme!
+
